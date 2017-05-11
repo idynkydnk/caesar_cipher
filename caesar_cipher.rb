fb52c21b98292b2a9b6a80d4038549ededdedf91
Kyle Thomson <idynkydnk@gmail.com>
2017-05-10 23:12:51 -0400

    centered everything

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -5,10 +5,10 @@ get '/' do
 end
 
 post '/' do
-  message = params[:message] || ""
+  message = params[:message] || " "
   offset = params[:offset] || 0
   new_message = caesar_cipher(message, offset.to_i)
-  erb :form, :locals => {'new_message' => new_message, 'message' => message, 'offset' => offset}
+  erb :index, :locals => {'new_message' => new_message, 'message' => message, 'offset' => offset}
 end
 
 def caesar_cipher text, offset
